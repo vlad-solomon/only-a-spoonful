993f8f55bd736b3eb61b8c6e7eaff256ec46dc1e
Vlad Solomon <vladsolomon720@gmail.com>
2025-03-18 20:35:14 +0200

    refactor

--- index.js
+++ index.js
@@ -62,34 +62,38 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
 	const userId = newState.member?.id;
 	if (!userId) return;
 
-	if (cooldowns.has(userId)) return;
-	cooldowns.set(userId, Date.now());
-	setTimeout(() => cooldowns.delete(userId), 5000);
+	if (oldState.selfDeaf && !newState.selfDeaf) {
+		if (cooldowns.has(userId)) return;
+		cooldowns.set(userId, Date.now());
+		setTimeout(() => cooldowns.delete(userId), 5000);
 
-	const connection = getVoiceConnection(newState.guild.id);
-	if (!connection) return;
+		console.log(`Playing Vine Boom sound for ${newState.member.user.tag}`);
+
+		const connection = getVoiceConnection(newState.guild.id);
+		if (!connection) return;
+
+		const player = createAudioPlayer();
+		const resource = createAudioResource(path.join(__dirname, "vineboom.mp3"));
+
+		player.play(resource);
+		connection.subscribe(player);
+
+		player.on(AudioPlayerStatus.Idle, () => {
+			console.log("Finished playing Vine Boom sound. Cleaning up player.");
+			player.stop();
+		});
+	}
 
 	if (oldState.channelId && !newState.channelId) {
+		const connection = getVoiceConnection(oldState.guild.id);
+		if (!connection) return;
+
 		const channel = oldState.guild.channels.cache.get(oldState.channelId);
 		if (channel && channel.members.filter((member) => !member.user.bot).size === 0) {
 			console.log("Channel is empty. Disconnecting bot.");
 			connection.destroy();
-			return;
 		}
 	}
-
-	console.log(`Playing Vine Boom sound for ${newState.member.user.tag}`);
-
-	const player = createAudioPlayer();
-	const resource = createAudioResource(path.join(__dirname, "boom.mp3"));
-
-	player.play(resource);
-	connection.subscribe(player);
-
-	player.on(AudioPlayerStatus.Idle, () => {
-		console.log("Finished playing Vine Boom sound. Cleaning up player.");
-		player.stop();
-	});
 });
 
 client.once("ready", () => {
